dc230f8c818b2dce84756fdf411f9197221dad50
laurieftn <laurie.ftn@gmail.com>
2020-11-09 11:10:54 +0100

    Envoi de l'exercice 2, partie 2

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(function(){ //
+    //$('elementCiblé').css('propriété','valeur'); 
+    $("#clickMe").click(function(){ // déclaration de la fonction qui permet d'ouvrir la boîte de dialogue au clic
+        alert("Bonjour !"); // Boîte de dialogue avec message
+        })
+});
